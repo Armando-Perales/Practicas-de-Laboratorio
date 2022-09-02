2c24b265cb6e5d35827621d4de56c83302986851
Armando-Perales <111466401+Armando-Perales@users.noreply.github.com>
2022-09-02 07:49:18 -0500

    Practica04 7:49
    Inicio 7:40

---
+++ practica04/Personaje.java
@@ -0,0 +1,21 @@
+public class Personaje{
+	private String nombre;
+	private int edad = 0;
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+	public String getNombre(){
+		return nombre;
+	}
+	public int getEdad(){
+		return edad;
+	}
+	public boolean setEdad(int edad){
+		if (edad > 0 && edad < 120) {
+			
+		}
+	}
+	public void saludar(){
+		System.out.println("Hola Alumno de POO "+ nombre);
+	}
+}
