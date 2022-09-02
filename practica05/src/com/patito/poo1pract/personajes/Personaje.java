0f6230ba454264066f109d49aca163b398076076
Armando-Perales <111466401+Armando-Perales@users.noreply.github.com>
2022-09-02 08:50:08 -0500

    practica05 85:0
    Inicio 8:40

---
+++ practica05/src/com/patito/poo1pract/personajes/Personaje.java
@@ -0,0 +1,29 @@
+package com.patito.poo1pract.personaje;
+public class Personaje{
+	private String nombre;
+	private int edad;
+	public Personaje(String nombre){
+		this.nombre = nombre;
+		this.edad = 0;
+	}
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+	public String getNombre(){
+		return nombre;
+	}
+	public int getEdad(){
+		return edad;
+	}
+	public boolean setEdad(int edad){
+		boolean resultadoEdad = false;
+		if (edad > 0 && edad < 120) {
+			resultadoEdad =  true;
+			this.edad = edad;
+		}
+		return resultadoEdad;
+	}
+	public void saludar(){
+		System.out.println("Hola Alumno de POO "+ nombre);
+	}
+}
---
+++ practica05/src/com/patito/poo1pract/prueba/PruebaPersonaje.java
@@ -0,0 +1,21 @@
+package com.patito.poo1pract.prueba;
+import com.patito.poo1pract.personaje.*;
+public class PruebaPersonaje{
+	public static void main(String[] args) {
+		Personaje alumno = new Personaje("Kal El");
+		alumno.saludar();
+		System.out.println("Nombre: " + alumno.getNombre() + " Edad: " + alumno.getEdad());
+		System.out.println("Modificando el nombre " + alumno.getNombre());
+		alumno.setNombre("Michael Jordan");
+		alumno.saludar();
+		if (!alumno.setEdad(30)) {
+			System.out.println("Edad sin cambio");
+		}
+		System.out.println("Nombre: " + alumno.getNombre() + " Edad: " + alumno.getEdad());
+		System.out.println("Modificando edad en 130");
+		if (!alumno.setEdad(130)) {
+			System.out.println("Edad sin cambio");
+		}
+		System.out.println("Nombre: " + alumno.getNombre() + " Edad: " + alumno.getEdad());
+	}
+}
