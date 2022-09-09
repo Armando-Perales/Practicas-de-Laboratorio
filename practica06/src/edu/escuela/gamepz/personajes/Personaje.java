69eb5d2bcaa9f3138066906017ca1213b1a34d6a
Armando-Perales <111466401+Armando-Perales@users.noreply.github.com>
2022-09-09 07:52:27 -0500

    practica06 7:52
    Inicio 7:40

---
+++ practica06/src/edu/escuela/gamepz/personajes/Personaje.java
@@ -0,0 +1,33 @@
+package com.patito.poo1pract.personajes;
+public class Personaje{
+	private String nombre;
+	private int edad;
+	public Personaje(String nombre, int edad){
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+	public void setNombre(String nombre){
+		int longitud = leng
+		if () {
+			
+		}
+		this.nombre = nombre;
+	}
+	public String getNombre(){
+		return nombre;
+	}
+	public int getEdad(){
+		return edad;
+	}
+	public boolean setEdad(int edad){
+		boolean resultadoEdad = false;
+		if (edad > 0 && edad < 120) {
+			resultadoEdad =  true;
+			this.edad = edad;
+		}
+		return resultadoEdad;
+	}
+	public void saludar(){
+		System.out.println("Hola Alumno de POO "+ nombre);
+	}
+}
