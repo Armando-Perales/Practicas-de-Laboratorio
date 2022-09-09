cf104d0b0b599bf9bdee5f004b391dba7646c851
Armando-Perales <111466401+Armando-Perales@users.noreply.github.com>
2022-09-09 09:01:27 -0500

    practica06 9:00

--- practica06/src/edu/escuela/gamepz/personajes/Personaje.java
+++ practica06/src/edu/escuela/gamepz/personajes/Personaje.java
@@ -1,10 +1,10 @@
 package edu.escuela.gamepz.personajes;
 public class Personaje{
 	private String nombre;
-	private int edad = 0;
-	public Personaje(String nombre, int edad){
+	private int edad;
+	public Personaje(String nombre){
 		this.nombre = nombre;
-		this.edad = setEdad(edad);
+		this.edad = 0;
 	}
 	public void setNombre(String nombre){
 		if (nombre.length() > 5 && nombre.length() < 25) {
