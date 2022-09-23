259f8a04c8900003c26abe06b898c74252975d69
Armando-Perales <111466401+Armando-Perales@users.noreply.github.com>
2022-09-23 07:42:47 -0500

    practica07 7:42 am
    Inicio 7:30 am

---
+++ practica07/src/edu/escuela/gamepz/personajes/Personaje.java
@@ -0,0 +1,35 @@
+package edu.escuela.gamepz.personajes;
+public class Personaje{
+	private String nombre;
+	private int edad;
+	public Personaje(String nombre){
+		this.nombre = nombre;
+		edad = 0;
+	}
+	public void setNombre(String nombre){
+		if (nombre.length() > 5 && nombre.length() < 25) {
+			this.nombre = nombre;
+		}
+	}
+	public String getNombre(){
+		return nombre;
+	}
+	public int getEdad(){
+		return edad;
+	}
+	public boolean setEdad(int edad){
+		boolean resultadoEdad = false;
+		if (edad > 0 && edad < 120) {
+			resultadoEdad =  true;
+			this.edad = edad;
+		}
+		return resultadoEdad;
+	}
+	public void saludar(){
+		System.out.println("Hola Alumno de POO "+ nombre);
+	}
+	public String getDetalle(){
+		String detalle = nombre + "\t"+ edad;
+		return detalle;
+	}
+}
---
+++ practica07/src/edu/escuela/gamepz/prueba/PruebaCiclos.java
@@ -0,0 +1,17 @@
+package edu.escuela.gamepz.prueba;
+import edu.escuela.gamepz.personajes.Personaje;
+public class PruebaCiclos{
+	public static void main(String[] args) {
+		Personaje per01 = new Personaje("Diego");
+		Personaje per02 = new Personaje("Almendra");
+		Personaje per03 = new Personaje("Bianca");
+		Personaje per04 = new Personaje("Jordan");
+
+		int n = 0;
+		int m = 0;
+		while (n >= m){
+			n = ((int) (Math.random()*100));
+			m = ((int) (Math.random()*100))
+		}
+	}
+}
