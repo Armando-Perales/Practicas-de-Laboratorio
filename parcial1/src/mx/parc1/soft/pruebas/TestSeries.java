7aa8bdf2e916dc7c79ff94352831c7c8beb7d7e8
Armando-Perales <111466401+Armando-Perales@users.noreply.github.com>
2022-09-30 09:06:18 -0500

    Parcial1 9:06 am

--- parcial1/src/mx/parc1/soft/pruebas/TestSeries.java
+++ parcial1/src/mx/parc1/soft/pruebas/TestSeries.java
@@ -1,11 +1,11 @@
 package mx.parc1.soft.pruebas;
 import mx.parc1.soft.series.Serie;
 public class TestSeries {
-    private int n;
     public static void main(String[] args) {
         Serie s = new Serie();
         /*
         int m = s.genNumero(100);
+        
         s.primos(m);
         
         int o = s.primoN(m/10);
@@ -14,14 +14,7 @@ public class TestSeries {
         int n = s.genNumero(100);
         s.fibo(n);
         */
-        int p = s.fiboN(3);
+        int p = s.fiboN(13);
         System.out.println(p);
-    }
-    public int genNumero(int x){
-        if (x < 10) {
-            n = 100
-            return n;
-        }
-        n = (int) (Math.random()*x*10);
     }
 }
--- parcial1/src/mx/parc1/soft/series/Serie.java
+++ parcial1/src/mx/parc1/soft/series/Serie.java
@@ -2,8 +2,17 @@ package mx.parc1.soft.series;
 public class Serie{
 	private int inicio;
 	private int limite;
+    //private int n;
 	private int fibonachi;
-
+	/*
+	public int genNumero(int x){
+        if (x < 10) {
+            n = 100;
+            return n;
+        }
+        n = (int) (Math.random()*x*10);
+    }
+    */
 	public int fiboN(int limite){
 		this.limite = limite;
 		inicio=1;
