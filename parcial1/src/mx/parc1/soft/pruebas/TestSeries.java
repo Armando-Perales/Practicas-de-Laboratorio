df69e0883147363b8bdda1a959436f61ddca46d7
Armando-Perales <111466401+Armando-Perales@users.noreply.github.com>
2022-09-30 09:10:25 -0500

    Parcial1 9:10 am

--- parcial1/src/mx/parc1/soft/pruebas/TestSeries.java
+++ parcial1/src/mx/parc1/soft/pruebas/TestSeries.java
@@ -3,9 +3,10 @@ import mx.parc1.soft.series.Serie;
 public class TestSeries {
     public static void main(String[] args) {
         Serie s = new Serie();
-        /*
-        int m = s.genNumero(100);
         
+        int m = s.genNumero(100);
+        System.out.println(m);
+        /*
         s.primos(m);
         
         int o = s.primoN(m/10);
@@ -14,7 +15,7 @@ public class TestSeries {
         int n = s.genNumero(100);
         s.fibo(n);
         */
-        int p = s.fiboN(13);
+        int p = s.fiboN(m);
         System.out.println(p);
     }
 }
--- parcial1/src/mx/parc1/soft/series/Serie.java
+++ parcial1/src/mx/parc1/soft/series/Serie.java
@@ -4,7 +4,7 @@ public class Serie{
 	private int limite;
     //private int n;
 	private int fibonachi;
-	/*
+	
 	public int genNumero(int x){
         if (x < 10) {
             n = 100;
@@ -12,12 +12,12 @@ public class Serie{
         }
         n = (int) (Math.random()*x*10);
     }
-    */
+    
 	public int fiboN(int limite){
 		this.limite = limite;
 		inicio=1;
 		fibonachi = 0;
-		for (int i = 0; i < limite ; i++ ) {
+		for (int i = 0; i < limite-1 ; i++ ) {
 			inicio += fibonachi;
 			fibonachi = inicio - fibonachi; 
 		}
