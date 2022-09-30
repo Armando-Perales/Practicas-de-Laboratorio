dd78c4ca93bba25bb5c59badc7b36b9367edb44c
Armando-Perales <111466401+Armando-Perales@users.noreply.github.com>
2022-09-30 08:52:54 -0500

    Parcial1 8:52 am

--- parcial1/src/mx/parc1/soft/pruebas/TestSeries.java
+++ parcial1/src/mx/parc1/soft/pruebas/TestSeries.java
@@ -1,6 +1,7 @@
 package mx.parc1.soft.pruebas;
-import mx.parc1.soft.series.*;
+import mx.parc1.soft.series.Serie;
 public class TestSeries {
+    private int n;
     public static void main(String[] args) {
         Serie s = new Serie();
         /*
@@ -15,5 +16,12 @@ public class TestSeries {
         */
         int p = s.fiboN(3);
         System.out.println(p);
+    }
+    public int genNumero(int x){
+        if (x < 10) {
+            n = 100
+            return n;
+        }
+        n = (int) (Math.random()*x*10);
     }
 }
