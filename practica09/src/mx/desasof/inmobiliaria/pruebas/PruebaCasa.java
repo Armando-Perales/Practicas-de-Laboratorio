16f45b98da4f4adb75ad5ed73e839db8f3be9416
Armando-Perales <111466401+Armando-Perales@users.noreply.github.com>
2022-10-14 10:21:21 -0500

    practica09 10:21 am

---
+++ practica09/src/mx/desasof/inmobiliaria/pruebas/PruebaCasa.java
@@ -0,0 +1,21 @@
+package mx.desasof.inmobiliaria.pruebas;
+import mx.desasof.inmobiliaria.casas.*;
+public class PruebaCasa{
+	public static void main(String[] args) {
+		Casa[] casas = {new Casa("Estandar",3,3000000),
+						new Campo("Cazador",3.5f,10f),
+						new Urbana("Residencia",3,2500000,2),
+						new Casa("Estandar",3,3000000),
+						new Campo("Cazador",3.5f,10f),
+						new Urbana("Residencia",3,2500000,2),
+						new Casa("Estandar"),
+						new Campo("Cazador",4,15000,3f,5f),
+						new Urbana("Residencia",5)};
+		int i = 0;
+		while (i < casas.length) {
+			for (Casa c : casas ) {
+			}
+			i++;
+		}
+	}
+}
