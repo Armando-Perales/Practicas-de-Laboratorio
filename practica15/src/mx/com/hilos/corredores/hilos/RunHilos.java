e5e0d1b323f93ab759ac804efdf332ab7ce00f37
Armando-Perales <111466401+Armando-Perales@users.noreply.github.com>
2022-11-25 09:00:43 -0600

    practica15 9:00 am

--- practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
+++ practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
@@ -1,11 +1,18 @@
 package mx.com.hilos.corredores.hilos;
-import mx.com.hilos.corredores.ventanas;
+import mx.com.hilos.corredores.ventanas.Ventana;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 public class RunHilos{
 	JTextField txt1, txt2;
 	JLabel elem1, elem2;
 	public void iniciar(){
-		
+		char ch1 = (txt1.getText().lenght() == 0) ? 'E' : txt1.getText().charAt(0);
+		char ch2 = (txt2.getText().lenght() == 0) ? 'M' : txt2.getText().charAt(0);
+		String salida1 = ch1;
+		String salida2 = ch2;
+		elem1.setText(salida1);
+		elem2.setText(salida2);
+		Letra hilo1 = new Letra(elem1,ch1);
+		Letra hilo2 = new Letra(elem2,ch2);
 	}
 }
