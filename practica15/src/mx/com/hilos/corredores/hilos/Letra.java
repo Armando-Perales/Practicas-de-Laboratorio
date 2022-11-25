f206302b6b120d05d852dcd247b1d8193bae6ba4
Armando-Perales <111466401+Armando-Perales@users.noreply.github.com>
2022-11-25 10:04:32 -0600

    practica15 10:04 am

--- practica15/src/mx/com/hilos/corredores/hilos/Letra.java
+++ practica15/src/mx/com/hilos/corredores/hilos/Letra.java
@@ -1,2 +1,25 @@
+package mx.com.hilos.corredores.hilos;
+import javax.swing.JLabel;
 public class Letra implements Runnable{
+	char letra;
+	JLabel etq;
+	public Letra(JLabel etq, char letra){
+		this.etq = etq;
+		this.letra = letra;
+	}
+	public void run(){
+		String cadena = "";
+		for (int i=0; i < 50 ; i++ ) {
+			cadena += letra;
+			etq.setText(cadena);
+			int rand = (int)(Math.random()*1000);
+			try{
+				Thread.sleep(rand);
+			}catch (InterruptedException e) {
+				
+			}
+		}
+		cadena += "Finalizado";
+		etq.setText(cadena);
+	}
 }
--- practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
+++ practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
@@ -4,23 +4,21 @@ import mx.com.hilos.corredores.ventanas.Ventana;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 public class RunHilos{
-	JTextField txt1, txt2;
-	JLabel elem1, elem2;
-	public void iniciar(){
+	public static void iniciar(JTextField txt1,JTextField txt2,JLabel elem1,JLabel elem2){
 		char ch1;
 		char ch2;
-		if (txt1.getText().lenght() == 0) {
+		if (txt1.getText().length() == 0) {
 			ch1 = 'M';
 		}
-		ch1 = (txt1.getText().charAt() == " ") ? 'M' : txt1.getText().charAt(0);
+		ch1 = (txt1.getText().charAt(0) == ' ') ? 'M' : txt1.getText().charAt(0);
 
-		if (txt2.getText().lenght() == 0) {
+		if (txt2.getText().length() == 0) {
 			ch2 = 'M';
 		}
-		ch2 = (txt2.getText().charAt() == " ") ? 'M' : txt2.getText().charAt(0);
+		ch2 = (txt2.getText().charAt(0) == ' ') ? 'M' : txt2.getText().charAt(0);
 
-		String salida1 = ch1;
-		String salida2 = ch2;
+		String salida1 = ""+ch1;
+		String salida2 = ""+ch2;
 		elem1.setText(salida1);
 		elem2.setText(salida2);
 		Letra hilo1 = new Letra(elem1,ch1);
